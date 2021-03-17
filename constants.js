a3afc2278a8aa5b101fe7f696ccd22b6f4b608ad
Dan Fey <danf@safecorp.com>
2021-03-17 13:46:44 -0700

    Using a5c3662 kafkajs commit

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 module.exports = {
   BROKER_LIST: ["localhost:9092"],
   DELAY_PER_MESSAGE_MS: 250,
-  GROUP_ID: "group.32",
+  GROUP_ID: "group.44",
   NUM_MESSAGES: 1200,
   OUT_FILE: "results.csv",
   PARTITIONS: 6,
-  TOPIC: "rebalance.test.36",
+  TOPIC: "rebalance.test.44",
 }
